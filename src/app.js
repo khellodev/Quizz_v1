9d46bfe198574c91122e6da1babae12c8d2b991f
fabrice clamens <fabrice.clamens@gmail.com>
2023-02-14 16:56:04 +0100

    version de mardi

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
-fetch;
-fetch("quizz.json")
+fetch("/question.json")
   .then((response) => {
     return response.json();
   })
   .then((json) => {
     console.log(json);
   });
+// console.log("hello");
+
+(async () => {
+  const response = await fetch("/question.json");
+  const json = await response.json();
+  document.getElementById("question").innerHTML = json.question;
+  document.getElementById("rep1").innerHTML = json.reponses[0].libelle;
+  document.getElementById("rep2").innerHTML = json.reponses[1].libelle;
+  document.getElementById("rep3").innerHTML = json.reponses[2].libelle;
+  document.getElementById("rep4").innerHTML = json.reponses[3].libelle;
+})();
+// (json.phoneNumber[1].type);
