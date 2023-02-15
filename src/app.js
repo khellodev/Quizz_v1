17393bea803e7f360412533a62030f54b3a6bc4b
fabrice clamens <fabrice.clamens@gmail.com>
2023-02-15 12:23:59 +0100

    mercredi matin affichage des questionet des quizzs en liste

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+// je vais chercher les questions
 fetch("/question.json")
   .then((response) => {
     return response.json();
@@ -5,8 +6,8 @@ fetch("/question.json")
   .then((json) => {
     console.log(json);
   });
-// console.log("hello");
 
+// jaffiche les questions
 (async () => {
   const response = await fetch("/question.json");
   const json = await response.json();
@@ -16,4 +17,32 @@ fetch("/question.json")
   document.getElementById("rep3").innerHTML = json.reponses[2].libelle;
   document.getElementById("rep4").innerHTML = json.reponses[3].libelle;
 })();
-// (json.phoneNumber[1].type);
+// copyright
+document.getElementById("copy").innerHTML =
+  "© QuizzMe - 2020 -" + new Date().getFullYear();
+
+// *************************************************
+// je vais chercher les quizzs
+fetch("/quizz2.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((json) => {
+    console.log(json);
+  });
+
+// jaffiche les quizzs
+(async () => {
+  const response = await fetch("/quizz2.json");
+  const json = await response.json();
+  console.log(json);
+
+  console.log(json.name[0]);
+  console.log(json.name[1]);
+
+  let nodeList = document.querySelectorAll("li");
+  nodeList[0].innerHTML = json.name[0];
+  // let nodeList = document.querySelectorAll("li");
+  // nodeList[0].innerHTML = json.name[0];
+  // document.querySelectorAll()
+})();
